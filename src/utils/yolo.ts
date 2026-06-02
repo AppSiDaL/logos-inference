70a0762723df3ab571996f8cd192708b4ad43601
David Martinez Cruz <david@MacBook-Air-de-David.local>
2026-06-01 19:44:38 -0600

    Actualiza modelo YOLO Nano ONNX

--- src/utils/yolo.ts
+++ src/utils/yolo.ts
@@ -4,7 +4,7 @@ let session: ort.InferenceSession
 
 export const loadModel = async () => {
   session = await ort.InferenceSession.create(
-    '/models/best.onnx'
+    '/models/best_nano.onnx'
   )
 
   console.log('Modelo cargado')
